866d2f88be6e877e88828ceaed12d2754ffcc3ef
Dhanajay Mehta <dmehta@HW12400.local>
2018-10-11 23:56:23 -0700

    updating readme and v-env

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-# user_defined parameters  sequence - 0: file location, 1: is_html[True, False], 2:text_stemming[True, False], 3:ft_dim, 4: N-cluster
+# user_defined parameters  sequence - 1: file location, 2:is_html[True, False], 3:text_stemming[True, False], 4:ft_dim, 5: N-cluster
+#
+# Parm 2, 3 - These are the Parameters for text pre-processing
+# Parm 4 - This is the parameter for Fasttext dimension
+# Parm 5 - This is parameter for the number of cluster needed in task 3
 #
 python3 ./app/main.py ./input/Papers.csv False True 300 10
